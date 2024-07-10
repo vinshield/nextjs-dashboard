2d5081c4099d11be321d40c4890a23fd70cbc45f
Jesse <jeslelacodes@gmail.com>
2024-07-11 00:32:03 +0100

    Effect search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,17 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    console.log(`Searching... ${term}`);
 
     if (term) {
       params.set('query', term);
@@ -20,7 +23,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
 
     // console.log([...params]);
-  }
+  }, 400);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
